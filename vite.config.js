81af13ed0e9b5d256de3217ef1aba17095a93d3f
Elias Aguilera <200393648@student.georgianc.on.ca>
2025-12-30 11:23:06 -0300

    Set Vite base to '/' for custom domain (fix blank page)

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/minimarketdonjuan/',
-  plugins: [
-    tailwindcss(),
-    react()],
+  base: '/',
+  plugins: [tailwindcss(), react()],
 })
